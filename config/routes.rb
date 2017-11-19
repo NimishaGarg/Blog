7fc7cbdda3113cbff59b94e265221335df6d78dc
NimishaGarg <nimishagarg.ng@gmail.com>
2017-11-19 16:57:54 +0530

    First Commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'users/show'
+
+  #devise_for :users
+	devise_for :users, :controllers => { registrations: 'registrations' }
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :posts
+  root 'posts#index'
+
+	resources :users
+end
